43f0efd9d676e13c09faa634794b12d8e836fdbc
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-10-24 21:03:48 -0300

    Home page settings and styles

--- functions.php
+++ functions.php
@@ -26,8 +26,16 @@ add_action( 'wp_enqueue_scripts', 'chilevapea_scripts' );
 // Register widget
 function topbar_widgets_init() {
 	register_sidebar( array(
-		'name'          => 'Topbar Widget',
-		'id'            => 'topbar_widget',
+		'name'          => 'Topbar Widget Right',
+		'id'            => 'topbar_widget_right',
+		'before_widget' => '<div  class="section">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => 'Topbar Widget Left',
+		'id'            => 'topbar_widget_left',
 		'before_widget' => '<div  class="section">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="title">',
@@ -45,15 +53,24 @@ function topbar_widgets_init() {
 add_action( 'widgets_init', 'topbar_widgets_init' );
 
 // Add to top bar
-function topbar_widget( $content ) {
-	if ( is_active_sidebar( 'topbar_widget' ) && is_main_query() ) {
-		echo '<div id="topbar">';
-		dynamic_sidebar( 'topbar_widget' );
+function topbar_widget_right( $content ) {
+	echo '<div id="topbar">';
+	if ( is_active_sidebar( 'topbar_widget_left' ) && is_main_query() ) {
+		echo '<div id="topbar-left">';
+		dynamic_sidebar( 'topbar_widget_left' );
 		echo '</div>';
 	}
+	if ( is_active_sidebar( 'topbar_widget_right' ) && is_main_query() ) {
+		echo '<div id="topbar-right">';
+		dynamic_sidebar( 'topbar_widget_right' );
+		echo '<div class="section">';
+		storefront_product_search();
+		echo '</div></div>';
+	}
+	echo '</div>';
 	return $content;
 }
-add_filter( 'storefront_before_header', 'topbar_widget' );
+add_filter( 'storefront_header', 'topbar_widget_right' );
 
 // Add Home Banner
 
@@ -65,5 +82,53 @@ function home_banner_widget( $content ) {
 	}
 	return $content;
 }
-
 add_filter( 'storefront_before_content', 'home_banner_widget' );
+
+// Remove header cart
+
+function remove_sf_actions() {
+	remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+	remove_action( 'storefront_header', 'storefront_product_search', 40 );
+	remove_action( 'homepage', 'storefront_homepage_content', 5 );
+	remove_action( 'homepage', 'storefront_popular_products', 5 );
+	remove_action( 'homepage', 'storefront_on_sale_products', 5 );
+	remove_action( 'homepage', 'storefront_best_selling_products', 5 );
+	remove_action( 'storefront_content_top', 'storefront_shop_messages', 15 );
+	remove_action( 'storefront_homepage', 'storefront_homepage_header', 10 );
+	remove_action( 'storefront_homepage', 'storefront_page_content', 20 );
+}
+add_action( 'init', 'remove_sf_actions', 1 );
+
+function tp_homepage_blocks_custom() {
+	/* Shop by Category */
+	add_filter( 'storefront_product_categories_args', function( $args ) {
+		$args = array(
+			'limit'   => 5,
+			'columns' => 5,
+			'title' => __( 'Categorías', 'storefront' ),
+		);
+		return $args;
+	} );
+
+	/* New In */
+	add_filter( 'storefront_recent_products_args', function( $args ) {
+		$args = array(
+			'limit'   => 5,
+			'columns' => 5,
+			'title'   => __( 'Productos Recientes', 'storefront' ),
+		);
+		return $args;
+	} );
+	/* Best Sellers */
+	add_filter( 'storefront_best_selling_products_args', function( $args ) {
+		$args = array(
+			'limit'   => 5,
+			'columns' => 5,
+			'title'   => __( 'Mejores Vendidos', 'storefront' ),
+		);
+		return $args;
+	} );
+
+	/* And so on.... */
+}
+add_action( 'after_setup_theme', 'tp_homepage_blocks_custom' );
